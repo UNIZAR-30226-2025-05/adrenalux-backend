80020730e9504cf0b1a43320b45d693dcc959022
Marcos1236 <marcosgomamartinez@gmail.com>
2025-02-14 19:57:59 +0100

    Corregida sintaxis de Drizzle y añadida carpeta de migraciones

--- api/db/schemas/amistad.js
+++ api/db/schemas/amistad.js
@@ -1,18 +1,26 @@
-import { pgTable, varchar, integer, foreignKey } from 'drizzle-orm/pg-core';
+import { text, varchar, pgTable, primaryKey } from 'drizzle-orm/pg-core';
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
+import { users } from './user.js';
 
-export const amistad = pgTable('amistad', {
-  user1_id: varchar('user1_id', { length: 50 }).notNull(),
-  user2_id: varchar('user2_id', { length: 50 }).notNull(),
-  created_at: text('created_at').notNull().default(new Date().toISOString()),
-  
-  user1: foreignKey('user1_id').references(users.username),
-  user2: foreignKey('user2_id').references(users.username),
+export const amistad = pgTable(
+  {
+    user1_id: varchar('user1_id', { length: 50 })
+      .notNull()
+      .references(() => users.username),
+    user2_id: varchar('user2_id', { length: 50 })
+      .notNull()
+      .references(() => users.username),
+    created_at: text('created_at')
+      .notNull()
+      .default(new Date().toISOString()),
+  },
+  (table) => ({
+    compuesta: primaryKey({
+      columns: [table.user1_id, table.user2_id]
+    })
+  })
+);
 
-  primaryKey: pgTable.primaryKey('user1_id', 'user2_id')
-});
-
-// Esquemas para insertar y seleccionar
 export const amistadSelectSchema = createSelectSchema(amistad).partial();
 export const amistadInsertSchema = createInsertSchema(amistad).partial();
 
