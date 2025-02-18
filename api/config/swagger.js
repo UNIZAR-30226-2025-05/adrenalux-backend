75f760359f5bfa2b874914e366179e8228f54ed6
Marcos1236 <marcosgomamartinez@gmail.com>
2025-02-18 15:19:18 +0100

    Corregir commit anterior

--- api/config/swagger.js
+++ api/config/swagger.js
@@ -1,7 +1,7 @@
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
-const swaggerOptions = {
+const options = {
   swaggerDefinition: {
     info: {
       title: "API",
