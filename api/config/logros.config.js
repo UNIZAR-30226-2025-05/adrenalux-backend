fb91b66743173e23ff214347bebb210d77ad27bd
Marcos1236 <marcosgomamartinez@gmail.com>
2025-03-06 11:35:58 +0100

    Arreglar script de generación de logros

--- api/config/logros.config.js
+++ api/config/logros.config.js
@@ -1,8 +1,15 @@
 export const TIPOS_DE_LOGROS = {
-    PARTIDAS_JUGADAS: 'Partidas jugadas',
-    PARTIDAS_GANADAS: 'Partidas ganadas',
-    SOBRES_ABIERTOS: 'Sobres abiertos',
-    CARTAS_CONSEGUIDAS: 'Cartas conseguidas',
-    NIVEL_ALCANZADO: 'Nivel alcanzado',
-  
-  }
+    PARTIDAS_JUGADAS: 1,
+    PARTIDAS_GANADAS: 2,
+    SOBRES_ABIERTOS: 3,
+    CARTAS_CONSEGUIDAS: 4,
+    NIVEL_ALCANZADO: 5,
+}
+
+export const DESCRIPCION_LOGROS = {
+  PARTIDAS_JUGADAS: 'Partidas jugadas',
+  PARTIDAS_GANADAS: 'Partidas ganadas',
+  SOBRES_ABIERTOS: 'Sobres abiertos',
+  CARTAS_CONSEGUIDAS: 'Cartas conseguidas',
+  NIVEL_ALCANZADO: 'Nivel alcanzado',
+}
--- api/scripts/loadLogros.js
+++ api/scripts/loadLogros.js
@@ -2,14 +2,14 @@
 
 import { db } from '../config/db.js'; 
 import { logro } from '../db/schemas/logro.js'; 
-import { TIPOS_DE_LOGROS } from '../config/logros.config.js';
+import { DESCRIPCION_LOGROS, TIPOS_DE_LOGROS } from '../config/logros.config.js';
 import { RECOMPENSAS } from '../config/recompensas.config.js'; 
 
-export async function generarLogros(cantidad, tipoLogro, incremento, recompensaTipo, recompensaCantidad) {
+export async function generarLogros(cantidad, tipoLogro, descripcion, incremento, recompensaTipo, recompensaCantidad) {
   const logros = [];
 
   for (let i = 1; i <= cantidad; i++) {
-    const description = `Logro por ${i * incremento} ${tipoLogro.toLowerCase()}`;
+    const description = `Logro por ${i * incremento} ${descripcion.toLowerCase()}`;
     const requiredAmount = i * incremento;
 
     logros.push({
@@ -39,9 +39,9 @@ async function insertarLogros(logros) {
 
 (async () => {
   try {
-    await generarLogros(5, TIPOS_DE_LOGROS.PARTIDAS_JUGADAS, 10, 'DINERO', RECOMPENSAS.DINERO.GANAR_PARTIDA);
-    await generarLogros(5, TIPOS_DE_LOGROS.SOBRES_ABIERTOS, 5, 'EXPERIENCIA', RECOMPENSAS.EXPERIENCIA.ABRIR_SOBRE);
-    await generarLogros(5, TIPOS_DE_LOGROS.CARTAS_CONSEGUIDAS, 50, 'DINERO', RECOMPENSAS.DINERO.SUBIR_NIVEL);
+    await generarLogros(5, TIPOS_DE_LOGROS.PARTIDAS_JUGADAS, DESCRIPCION_LOGROS.PARTIDAS_JUGADAS, 10, 'DINERO', RECOMPENSAS.DINERO.GANAR_PARTIDA);
+    await generarLogros(5, TIPOS_DE_LOGROS.SOBRES_ABIERTOS, DESCRIPCION_LOGROS.SOBRES_ABIERTOS, 5, 'XP', RECOMPENSAS.EXPERIENCIA.ABRIR_SOBRE);
+    await generarLogros(5, TIPOS_DE_LOGROS.CARTAS_CONSEGUIDAS,  DESCRIPCION_LOGROS.CARTAS_CONSEGUIDAS, 50, 'DINERO', RECOMPENSAS.DINERO.SUBIR_NIVEL);
     console.log('Generación de logros completada.');
   } catch (error) {
     console.error('Error en la generación de logros:', error.message);
