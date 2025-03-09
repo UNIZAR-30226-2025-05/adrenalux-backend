43be983c9dd3574ae1bb8f9ccede3634620b4df4
Marcos1236 <marcosgomamartinez@gmail.com>
2025-03-09 10:09:14 +0100

    Cambiar gestión de aceptación de intercambios a websockets

--- api/controllers/socket.js
+++ api/controllers/socket.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid'; // Lo utilizaremos para generar IDs de sala
 import {socketAuth} from '../middlewares/socket_auth.js';
 
 const connectedUsers = new Map();
+const activeExchanges = new Map();
 
 export function configureWebSocket(server) {
   const io = new Server(server, {
@@ -15,13 +16,75 @@ export function configureWebSocket(server) {
   io.use(socketAuth);
 
   io.on('connection', (socket) => {
+
+    connectedUsers.set(String(socket.data.userID), socket);
+    
+    const { username } = socket.handshake.query; 
+    socket.data.username = username; 
+
     console.log(`Usuario conectado: ${socket.data.userID}`);
   
-    // Guardar conexión
-    connectedUsers.set(socket.data.userID, socket);
+    connectedUsers.set(String(socket.data.userID), socket);
+
+    socket.on('request_exchange', ({ receptorId, solicitanteUsername }) => {
+      const solicitanteId = String(socket.data.userID);
+      const exchangeId = `${solicitanteId}-${receptorId}`;
+  
+      if (solicitanteId === receptorId) {
+        return socket.emit('error', 'No puedes intercambiar contigo mismo');
+      }
+
+      const receptorSocket = connectedUsers.get(receptorId);
+      
+      if (!receptorSocket) {
+          return socket.emit('error', 'El usuario no está conectado');
+      }
+
+      console.log("Enviando invitacion de intercambio con id ", exchangeId);
+
+      activeExchanges.set(exchangeId, {
+          roomId: `exchange_${exchangeId}`,
+          participants: [solicitanteId, receptorId],
+          estado: 'pendiente'
+      });
+
+      receptorSocket.emit('request_exchange_received', {
+          exchangeId,
+          solicitanteId,
+          solicitanteUsername,
+          timestamp: new Date().toISOString()
+      });
+    });
+
+    socket.on('accept_exchange', (exchangeId) => {
+      const userId = String(socket.data.userID);
+      const intercambio = activeExchanges.get(exchangeId);
+  
+      if (!intercambio || intercambio.participants[1] !== userId) {
+        return socket.emit('error', 'Intercambio no válido');
+      }
+
+      const solicitanteSocket = connectedUsers.get(intercambio.participants[0]);
+      const receptorSocket = connectedUsers.get(intercambio.participants[1]);
+
+      const solicitanteUsername = solicitanteSocket?.data.username || 'Usuario';
+      const receptorUsername = receptorSocket?.data.username || 'Usuario';
+  
+      intercambio.estado = 'activo';
+      
+      solicitanteSocket.join(intercambio.roomId);
+      receptorSocket.join(intercambio.roomId);
+
+      io.to(intercambio.roomId).emit('exchange_accepted', {
+        exchangeId,
+        roomId: intercambio.roomId,
+        solicitanteUsername: solicitanteUsername,
+        receptorUsername: receptorUsername, 
+      });
+    });
     
     socket.on('disconnect', () => {
-      connectedUsers.delete(socket.data.userID);
+      connectedUsers.delete(String(socket.data.userID));
       console.log(`Usuario desconectado: ${socket.data.userID}`);
     });
 
@@ -32,7 +95,7 @@ export function configureWebSocket(server) {
 }
 
 export function sendNotification(toUserId, type, data) {
-  const targetSocket = connectedUsers.get(toUserId);
+  const targetSocket = connectedUsers.get(String(toUserId));
   if (targetSocket) {
     targetSocket.emit('notification', {
       message: data['message'],
@@ -46,5 +109,5 @@ export function sendNotification(toUserId, type, data) {
 }
 
 export function isConnected(userId) {
-  return connectedUsers.has(userId);
+  return connectedUsers.has(String(userId));
 }
