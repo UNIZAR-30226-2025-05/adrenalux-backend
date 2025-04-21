85c74bba128ab443ee1357980e665d5b6ef3c57c
PabloVilla199 <pablovillacamanes@gmail.com>
2025-04-21 18:41:50 +0200

    arreglar swaggwer

--- api/routes/partidas.js
+++ api/routes/partidas.js
@@ -4,50 +4,7 @@ import * as partidas from '../controllers/partidas.js';
 
 const router = express.Router();
 
-/**
- * @swagger
- * tags:
- *   name: Partidas
- *   description: Endpoints relacionados con las partidas
- */
 
-/**
- * @swagger
- * /partidas/pausadas:
- *   get:
- *     summary: Obtener las partidas pausadas de un usuario
- *     tags: [Partidas]
- *     security:
- *       - bearerAuth: []
- *     responses:
- *       200:
- *         description: Lista de partidas pausadas para el usuario
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 pausedMatches:
- *                   type: array
- *                   items:
- *                     type: object
- *                     properties:
- *                       id:
- *                         type: integer
- *                       estado:
- *                         type: string
- *                       user1_id:
- *                         type: integer
- *                       user2_id:
- *                         type: integer
- *                       fecha_creacion:
- *                         type: string
- *                         format: date-time
- *       401:
- *         description: No autorizado, falta de token válido
- *       500:
- *         description: Error interno del servidor
- */
 router.get('/pausadas', authenticate, partidas.getPartidasPausadas);
 
 export default router;
